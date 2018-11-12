b6a0b26f7041ab318ac01d3af8cb2b98330b3786
Pablo Arranz Ropero <Pablo.ArranzRopero@zooplus.com>
2018-11-12 16:25:30 +0100

    ngclass css-by-component...

--- 6. other/src/app/app.module.ts
+++ 6. other/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgStyleComponent } from './components/ng-style/ng-style.component';
+import { CssComponent } from './components/css/css.component';
+import { NgClassComponent } from './components/ng-class/ng-class.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NgStyleComponent
+    NgStyleComponent,
+    CssComponent,
+    NgClassComponent
   ],
   imports: [
     BrowserModule,
